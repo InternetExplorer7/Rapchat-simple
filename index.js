fa04def918b4986b9c51d4c1ac97b1274e28dc8e
Kaveh Khorram <kavehkhorram@Kavehs-MacBook-Pro.local>
2017-01-16 03:04:49 -0700

    switched ports

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app = express();
 
-console.log(__dirname + '/build');
-
 app.use(express.static(__dirname + '/build'));
 
-app.listen(3000 || process.env.PORT);
+app.listen(process.env.PORT || 3000);
