8b5e7b3c600a4ca15c6a0018b34c9913474d5304
Kaveh Khorram <kavehkhorram@Kavehs-MacBook-Pro.local>
2017-01-16 03:06:37 -0700

    new

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+console.log(__dirname + '/build');
+
 app.use(express.static(__dirname + '/build'));
 
 app.listen(process.env.PORT || 3000);
