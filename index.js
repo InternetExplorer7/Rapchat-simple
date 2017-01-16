bba330563d5558ab622a413f4ea11c2785eb9433
Kaveh Khorram <kavehkhorram@Kavehs-MacBook-Pro.local>
2017-01-16 03:12:58 -0700

    fsfs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var server = require('pushstate-server');
 
+console.log('server started on port ' + process.env.PORT);
+
 server.start({
     port: process.env.PORT || 8080,
     directory: './build'
